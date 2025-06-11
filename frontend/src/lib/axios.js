72b90cb1491d90224127614b12655863f2ee25d1
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-06-11 18:38:09 +0500

    correcting thr deployment url of backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE =="development"?"http://localhost:5000/api":"/api"
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5000/api"
+  : "https://language-exchange-8zc9-backend.vercel.app/api";
+
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
