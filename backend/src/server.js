c95f8befed8ec70392ee7f0e2f35f63dc73b3d32
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-06-11 19:10:17 +0500

    correcting url again ........

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ 
-  // origin: "http://localhost:5173",
+  origin: "https://language-exchange-upgr.vercel.app/",
   credentials: true, 
   methods: ["GET", "POST", "PUT", "DELETE"],
 })); 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5000/api"
-  : "https://language-exchange-8zc9-backend.vercel.app/api ";
+  : "https://language-exchange-8zc9-backend.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
