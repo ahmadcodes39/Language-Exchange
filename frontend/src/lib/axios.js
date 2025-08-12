b6daeff0faa66e01b36d247ff89ebac6a64fa61d
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-08-12 11:45:58 +0500

    updating url of backend on frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const BASE_URL = import.meta.env.MODE === "development"
 //   ? "http://localhost:5000/api"
 //   : "https://language-exchange-8zc9-backend.vercel.app/api";
-const BASE_URL  = "http://localhost:5000/api"
+const BASE_URL  = "https://language-exchange-psi.vercel.app/api"
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
